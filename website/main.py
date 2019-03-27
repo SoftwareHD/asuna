8840181c44dc908cd9a452c6935f4bf33d0c9890
yuka-tuka <discord.yuka.dev@gmail.com>
2019-03-27 16:41:14 -0300

    Versão (a 0.1.1 beta)
    Correções
    1° - outh2 do site da asuna
    2° - index do site
    3° - correções de url
    4° - adicionais (.css, .png)

--- website/main.py
+++ website/main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, session, redirect, jsonify
 from urllib.parse import quote
-import requests, api, time
+import requests, api, time, json
 from configs.config import config
 
 app = Flask(__name__)
@@ -9,9 +9,19 @@ app.config['SECRET_KEY'] = config["secret_key"]
 
 @app.route('/')
 def index():
-	url = str(config["url_authentication"]).format(config["id_bot"], quote(config["redirect"]), quote(config["scopes"]))
-	token = session.get('token')
-	return render_template('index.html', url=url, token=token)
+	if session.get('token') is None:
+	  url_authentication = str(config["url_app"]).format(config["id_bot"], quote(config["redirect"]), quote(config["scopes"]))
+	  url_bot = str(config["url_bot"]).format(config["id_bot"], quote(config["redirect_3"]), quote("bot"))
+	  url = {"url_bot":url_bot, "url_authentication":url_authentication}
+	  return render_template('index.html', url=url, title="Asuna - Index")
+	try:
+		user = api.get_info(session.get('token'))
+	except requests.exceptions.HTTPError:
+		return "Token invalido!"
+
+	url_bot = str(config["url_bot"]).format(config["id_bot"], quote(config["redirect_2"]), quote("bot"))
+	url = {"url_bot":url_bot}
+	return render_template('index.html', url=url, title="Asuna - Index", user=user)
 
 @app.route('/callback')
 def response():
@@ -24,7 +34,7 @@ def response():
 
 	session['token'] = res['access_token']
 	session['scopes'] = str(config["scopes"]).split(" ")
-	return redirect("/me")
+	return redirect("/")
 
 @app.route('/me')
 def me():
@@ -59,4 +69,4 @@ def logout():
 	return redirect('/')
 
 if __name__ == '__main__':
-	app.run(debug=True,port=port)
+	app.run(debug=True,port=8000)
